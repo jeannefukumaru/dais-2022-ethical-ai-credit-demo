88480c39d2a75f88f2243d248f0dbee8ae65b8ce
jeannefukumaru <fukumaruuu@gmail.com>
2022-06-27 12:17:40 +0800

    tweak data generation

--- sandbox.py
+++ sandbox.py
@@ -9,14 +9,16 @@ test_03 = pd.read_csv("./data/credit_test_01.csv")
 dat = pd.concat([train_03, test_03])
 
 from numpy.random import randint
-dat['SEX'] = randint(1, 3, dat.shape[0])
-dat['MARRIAGE'] = randint(1, 3, dat.shape[0])
-dat['EDUCATION'] = randint(1, 4, dat.shape[0])
-dat['AGE'] = randint(1, 100, dat.shape[0])
-dat['USER_ID'] = [uuid.uuid4() for _ in range(len(dat.index))]
-dat['LIMIT_BAL'] = randint(10000, 100000, dat.shape[0])
+dat['PAY_AMT1'] = 0
+dat['PAY_AMT2'] = 0
+dat['PAY_AMT3'] = 0
+dat['PAY_AMT4'] = 0
+dat['PAY_AMT5'] = 0
+dat['PAY_AMT6'] = 0
+
+dat['default'] = randint(0,2, dat.shape[0])
 
 dat.iloc[0:2501, :].to_csv("./data/credit_train_02.csv", index=False)
-dat.iloc[2500:, :].to_csv("./data/credit_test_02.csv", index=False)
+dat.iloc[2501:, :].to_csv("./data/credit_test_02.csv", index=False)
 
 
